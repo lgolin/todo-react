8ab779693943b135e2dac5c077a867d3a7738fd5
Lais Golin <laisgolin@gmail.com>
2022-09-11 13:27:17 +0100

    style TaskList section

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -5,10 +5,12 @@ export function TaskList() {
   return (
     <section>
       <div className={styles.taskListBox}>
-        <input className={styles.styleD} type="checkbox" />
-        <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+        <div className={styles.box}>
+          <input type="checkbox" />
+          <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+        </div>
         <button title="Deletar Task">
-          <Trash size={20} />
+          <Trash size={24} />
         </button>
       </div>
     </section>
