d85b0854e96560c833ea7bf347606269c7a9377f
Lais Golin <laisgolin@gmail.com>
2022-11-29 23:39:23 +0000

    delete previous app and create a new one using jsx

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import styles from './Header.module.css';
+import logo from '../assets/logo.png';
+import { Input } from './Input';
+
+export default function Header() {
+  return (
+    <>
+      <header className={styles.header}>
+        <img src={logo} alt="Logotipo do Todo" />
+      </header>
+      <Input />
+    </>
+  );
+}
---
+++ src/Components/Input.jsx
@@ -0,0 +1,9 @@
+import styles from './Input.module.css';
+
+export function Input() {
+  return (
+    <section>
+      <input type="text" placeholder="Adicione uma nova tarefa" />
+    </section>
+  );
+}
