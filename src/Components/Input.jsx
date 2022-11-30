be954f4f1d9750b70aa5f8c8b39bb08b16b44f75
Lais Golin <laisgolin@gmail.com>
2022-11-30 12:09:01 +0000

    add create button

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,9 +1,11 @@
+import { Button } from './Button';
 import styles from './Input.module.css';
 
 export function Input() {
   return (
     <section className={`${styles.section} container`}>
       <input type="text" placeholder="Adicione uma nova tarefa" />
+      <Button>Criar</Button>
     </section>
   );
 }
