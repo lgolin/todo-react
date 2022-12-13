d5f863301bcbb8ce4c327ef15243257c5999c0dc
Lais Golin <laisgolin@gmail.com>
2022-12-13 16:28:49 +0000

    updating commits

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import { Button } from './Button';
 import styles from './Input.module.css';
 
 export function Input() {
+  const [ativo, setAtivo] = useState(true);
+
   return (
     <section className={`${styles.section} container`}>
-      <input type="text" placeholder="Adicione uma nova tarefa" />
-      <Button>Criar</Button>
+      <input
+        className={styles.input}
+        type="text"
+        placeholder="Adicione uma nova tarefa"
+      />
+      <button onClick={() => setAtivo(!ativo)}>
+        {ativo ? 'Ativo' : 'Inativo'}
+      </button>
     </section>
   );
 }
--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -9,19 +9,26 @@ const tasks = [
     id: 2,
     content: 'task number2',
   },
+  {
+    id: 3,
+    content: 'New task number2',
+  },
 ];
 
 export function Task() {
   return (
-    <main className={`${styles.classList} container`}>
-      {tasks.map((task) => (
-        <>
-          <input type="checkbox" name={task.content} id={task.content} />
-          <label for={task.content} key={task.content}>
-            {task.content}
-          </label>
-        </>
-      ))}
-    </main>
+    <section className={styles.taskContainer}>
+      <main className={`${styles.classList} container`}>
+        {tasks.map((task) => (
+          <>
+            <input type="checkbox" name={task.content} id={task.content} />
+            <label for={task.content} key={task.content}>
+              {task.content}
+            </label>
+            <br />
+          </>
+        ))}
+      </main>
+    </section>
   );
 }
